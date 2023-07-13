87db641b28fc49e4ab3dd67401bc6caf1b291d13
Moinulilm10 <moinulilm10@gmail.com>
2023-07-13 19:50:04 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {} from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>food order</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
