f228617f19e5e3ba67fd45be3835356a9fc03826
Moinul Islam <moinulilm10@gmail.com>
2023-09-03 00:28:49 +0600

    making items remove

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,25 +5,29 @@ import CartContext from "../Context_Store/Cart-Context";
 import CartItem from "./CartItem";
 
 export const Cart = (props) => {
-  const cartCtnx = useContext(CartContext);
+  const cartCtx = useContext(CartContext);
 
-  const totalAmount = `$${cartCtnx.totalAmount.toFixed(2)}`;
-  const hasItem = cartCtnx.items.length > 0;
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {cartCtnx.items.map((item) => (
+      {cartCtx.items.map((item) => (
         <CartItem
           key={item.id}
           name={item.name}
           amount={item.amount}
           price={item.price}
-          onRemove={cartItemRemoveHandler}
-          onAdd={cartItemAddHandler}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
@@ -40,7 +44,7 @@ export const Cart = (props) => {
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        {hasItem && <button className={classes.button}>Order</button>}
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
